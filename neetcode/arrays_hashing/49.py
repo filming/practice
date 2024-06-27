21713a6e329c1459b894c634220297a526595ba6
Filming <79931565+filming@users.noreply.github.com>
2024-06-27 13:29:19 -0400

    add: solution #2 for leetcode problem "Group Anagrams"

--- neetcode/arrays_hashing/49.py
+++ neetcode/arrays_hashing/49.py
@@ -28,6 +28,35 @@ Approach:
 Notes:
 - TimSort algorithm (or sorted() in Python) has a time complexity of O(n log n).
 
+[ Solution #2 ]
+
+Time Complexity:
+- O(n * k)
+- n is the total amount of strings in strs
+- k is the average length of a string in strs
+- Iterating over each string in strs is O(n).
+- Creating a char occurrence list of a string is O(n).
+- Converting a list of ints to a tuple of ints is O(n).
+- Looking up the existance and/or updating values in a dict is O(1).
+
+Space Complexity:
+- O(n * k)
+- n is the total amount of strings in strs
+- k is the average length of a string in strs
+- Creating a char occurrence list of a string is O(n).
+- Converting a list of ints to a tuple of ints is O(n).
+- In the worst case, when comparing, we can find no common sorted anagrams which means our grouped_anagrams hashmap will have the same number of elements as the given strs list. O(n)
+
+Approach:
+- Create a hashmap that will store our grouped anagrams (call it grouped_anagrams). A string containing the sorted characters of an anagram will be the key and the associated value being a list containing strings that if sorted, would yield the same string as the key. Initialize this hashmap as empty.
+- Create a loop that will iterate through each string in the given strs list, starting from the first string and stopping after the last one.
+- For each string in the loop, find and store the char occurrences of the string (this can done by using a list containing 26 ints), convert this list to a tuple (call it curr_str_char_occurrences_tuple) instead of a list so it is immutable. (and thus will have a consistent hash). 
+- Check and see if curr_str_char_occurrences_tuple is in grouped_anagrams as a key. If it is, add the currently iterated string in strs to the list which is stored as the key's associated value. If it is not, add curr_str_char_occurrences_tuple as a key in grouped_anagrams with the associated value being a list containing currently iterated string in strs.
+- The return value will be a list of all the values stored in grouped_anagrams.
+
+Notes:
+- Creating a char occurrence using a 26-char list approach in order to sort through the chars of a string can bring our Sorting TC to O(n)
+
 '''
 
 class Solution:
@@ -43,6 +72,25 @@ class Solution:
             else:
                 grouped_anagrams[curr_str_sorted] = [curr_str]
                 
+        return grouped_anagrams.values()
+
+    # Solution 2
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grouped_anagrams = {}
+
+        for curr_str in strs:
+            curr_str_char_occurrences = [0] * 26
+
+            for curr_char in curr_str:
+                curr_str_char_occurrences[ord(curr_char) - 97] += 1
+            
+            curr_str_char_occurrences_tuple = tuple(curr_str_char_occurrences)
+
+            if curr_str_char_occurrences_tuple in grouped_anagrams:
+                grouped_anagrams[curr_str_char_occurrences_tuple].append(curr_str)
+            else:
+                grouped_anagrams[curr_str_char_occurrences_tuple] = [curr_str]
+
         return grouped_anagrams.values()
 
 def main():
